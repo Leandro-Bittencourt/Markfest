91559eb0d879c22d5f3ae1159ec6d300b11b9082
Leandro-Bittencourt <147086907+Leandro-Bittencourt@users.noreply.github.com>
2025-11-18 12:42:44 +0000

    tenativa 3

---
+++ markfest/src/main/java/com/example/markfest/MarkfestApplication.java
@@ -0,0 +1,13 @@
+package com.example.markfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkfestApplication.class, args);
+	}
+
+}
---
+++ markfest/src/test/java/com/example/markfest/MarkfestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
